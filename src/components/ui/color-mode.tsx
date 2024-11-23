7f9ce7be47002c80a08af981a5f664ef41935a49
Caio Silva <contato@caiosb.com>
2024-11-23 11:46:29 -0300

    fix(component): :bug: prevent wrong type

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -2,15 +2,21 @@
 
 import type { IconButtonProps } from "@chakra-ui/react";
 import { ClientOnly, IconButton, Skeleton } from "@chakra-ui/react";
+import { ThemeProviderProps } from "@emotion/react";
 import { ThemeProvider, useTheme } from "next-themes";
 import * as React from "react";
 import { LuMoon, LuSun } from "react-icons/lu";
 
-export interface ColorModeProviderProps {}
+export interface ColorModeProviderProps extends ThemeProviderProps {}
 
-export function ColorModeProvider(props: ColorModeProviderProps) {
+export function ColorModeProvider({
+  children,
+  ...props
+}: ColorModeProviderProps) {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange {...props} />
+    <ThemeProvider attribute="class" disableTransitionOnChange {...props}>
+      {children}
+    </ThemeProvider>
   );
 }
 
